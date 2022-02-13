09d3a90655f0b4f5c2c39fcdd1ee59d7faab6aae
190039004-rehan <190039004@kluniversity.in>
2022-02-14 00:26:58 +0530

    version-1

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {    public static void main(String[] args) {        System.out.println("Hello, World!");    }
+}
+[A[[A[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[cat > HelloWorld.java
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+
+
